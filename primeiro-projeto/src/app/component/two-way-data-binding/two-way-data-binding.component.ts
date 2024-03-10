9ab74608c0f52d5b17905da38a7083be7b5f62dd
Leandro Gonçalves <leandro.goncalves@linx.com.br>
2024-03-09 22:34:06 -0300

    refactor: Alterações de acordo com a aula.

--- primeiro-projeto/src/app/component/two-way-data-binding/two-way-data-binding.component.ts
+++ primeiro-projeto/src/app/component/two-way-data-binding/two-way-data-binding.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TwoWayDataBindingComponent implements OnInit {
 
+  nome: string = 'abc';
+
   constructor() { }
 
   ngOnInit() {
